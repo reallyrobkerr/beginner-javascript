16eaeb89047514f1f44e34e457b75769a99bffec
rob kerr <reallyrobkerr@gmail.com>
2020-10-06 03:29:23 -0400

    monday/tuesday october 5/6 progress

---
+++ exercises/29 - Events/events.js
@@ -0,0 +1,19 @@
+const butts = document.querySelector('.butts');
+const buttz = document.querySelector('.buttz');
+
+// naming callbacks with 'handle' is...HANDY 'cause it's indicative
+// of being an event handler function for an event listener
+function handleCleck() {
+  console.log('CLEEEECCCK');
+}
+
+const arrowFunctionExample = () => console.log('CLLLLEEEEECCKH');
+
+butts.addEventListener('click', handleCleck);
+// the handleCleck callback function here is BOUND to the butts element
+
+// butts.removeEventListener('click', handleCleck);
+// this will work since the reference to function handleCleck is bound
+// you CANNOT use an anonymous callback function with removeEventListener
+
+buttz.addEventListener('click', arrowFunctionExample);
